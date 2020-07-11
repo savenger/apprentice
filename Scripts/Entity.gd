e9e0914b21a40d42f1fb77a27ee77d811c6db778
Lennart <mailtolennart@gmx.net>
2020-07-11 16:57:31 +0200

    merge

--- Scripts/Entity.gd
+++ Scripts/Entity.gd
@@ -90,6 +90,9 @@ func damage_loop():
 					multiplier = 2
 				elif body.get("ELEMENT") == get("ELEMENT"):
 					multiplier = -1
+			else:
+				if body.name == "Spell":
+					pass
 			update_health(health - multiplier * body.get("DAMAGE"))
 			hit_stun = 10
 			knock_dir = global_transform.origin - body.global_transform.origin
