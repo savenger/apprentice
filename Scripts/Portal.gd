0008065caa26419130624fe26d02720c7ed7acdd
savenger <github@smahmood.de>
2020-07-12 00:31:12 +0200

    #6 portal support 2 enemies at a time and can be deactivated by attacking them

--- Scripts/Portal.gd
+++ Scripts/Portal.gd
@@ -8,14 +8,25 @@ func _init():
 	TYPE = "PORTAL"
 
 var active setget active_set
-var supported_monsters = 2
+const MAX_SUPPORTED_MONSTERS = 2
+var supported_monsters = MAX_SUPPORTED_MONSTERS
 
 func active_set(new_value):
 	active = new_value
 	if active:
+		health = MAX_HEALTH
 		$portal.texture = load("res://Assets/Graphics/portal.png")
+		supported_monsters = MAX_SUPPORTED_MONSTERS
 	else:
 		$portal.texture = load("res://Assets/Graphics/portal_inactive.png")
+		supported_monsters = 0
 
 func _on_Timer_timeout():
-	active = true
+	print("_on_Timer_timeout()")
+	active_set(true)
+
+func _physics_process(delta):
+	damage_loop()
+	if health == 0 and $Timer.is_stopped():
+		active_set(false)
+		$Timer.start()
