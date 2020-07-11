cbf3126be30e2bf13f2817f3102db2ba6392d524
Lennart <mailtolennart@gmx.net>
2020-07-11 21:47:31 +0200

    #9 spells

--- Scripts/Spell.gd
+++ Scripts/Spell.gd
@@ -1,11 +1,8 @@
 extends RigidBody2D
 
-var speed = 800
-var DAMAGE = 10
+var ELEMENT
+var SPEED
+var DAMAGE
 
 func _ready():
-	apply_impulse(Vector2(), Vector2(speed, 0).rotated(get_angle_to(get_global_mouse_position())))
-
-func _on_Spell_body_entered(body):
-	if body.name != "Player":
-		queue_free()
+	apply_impulse(Vector2(), Vector2(SPEED, 0).rotated(get_angle_to(get_global_mouse_position())))
