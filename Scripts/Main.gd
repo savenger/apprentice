1eb158cc070c95113936c98d35c758223f386e49
savenger <github@smahmood.de>
2020-07-12 16:02:16 +0200

    #32 enemies can still be spawned but only the types of the available portal elements

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -8,15 +8,15 @@ var spawning = preload("res://Scenes/Spawning.tscn")
 func _process(delta):
 	var rng = RandomNumberGenerator.new()
 	rng.randomize()
-	var supported_monsters = 0
+	var supported_monsters = []
 	var enemies = 0
 	for node in get_children():
 		if node.name.find("Spawning") >= 0 or node.name.find("Enemy") == 0 or node.name.find("Enemy") == 1 and node.name.find("Tile") == -1 and node.name.find("Spell") == -1:
 			enemies += 1
 		elif node.name.find("Portal") >= 0:
-			supported_monsters += node.get("supported_monsters")
-	if enemies < min(supported_monsters, MAX_ENEMIES):
+			supported_monsters.append(node.get("ELEMENT"))
+	if enemies < MAX_ENEMIES:
 		var spawn = spawning.instance()
-		spawn.enemy_type = rng.randi() % 4
+		spawn.enemy_type = supported_monsters[rng.randi() % len(supported_monsters)]
 		add_child(spawn)
 		enemies += 1
